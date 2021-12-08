ea210bc83f44b8565e8e2a59b16e8090a0014adf
William Angulo <wmavalle@gmail.com>
2021-12-07 23:56:19 -0500

    feat: middelware addition

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 
-app.get('/home',(req,res)=>{
-    res.send("Home")
+const customMiddleare = (req,res,next)=> {
+    console.log("middleare executed!!")
+    next()
+}
+
+
+app.get('/',(req,res)=>{
+   console.log("Home")
+    res.send("hello word")
 })
+
+app.get('/about',customMiddleare,(req,res)=>{
+    console.log("About page")
+    res.send("about")
+})
+
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
 })
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+
+## Content
+[Middeleware functions] - Las funciones de middleware son funciones que tienen acceso al objeto de solicitud (req), al objeto de respuesta (res) y a la siguiente función de middleware en el ciclo de solicitud/respuestas de la aplicación. La siguiente función de middleware se denota normalmente con una variable denominada next.
+
